f0561164bb90db66d31bb218f2dc4254594bb766
Tommi Linnakangas <tkl@iki.fi>
2016-11-04 20:31:40 +0200

    Simplest web server with iris

---
+++ webserver.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+func main() {
+    api := iris.New()
+    api.Get("/", hello)
+    api.Listen(":8888")
+}
+
+func hello(ctx *iris.Context){
+   ctx.Write("Hello web!")
+}
